e8bd6038a04e191e1062ac6463064ec7fa19562e
Unknown <victorgesuato3@gmail.com>
2018-09-26 15:47:21 -0400

    AddingActionBarFunctionalityinDetailMealPortionActivity

--- app/src/main/java/com/cryptog/projectmealprepandroid/ui/DetailDailyPortionActivity.kt
+++ app/src/main/java/com/cryptog/projectmealprepandroid/ui/DetailDailyPortionActivity.kt
@@ -1,7 +1,10 @@
 package com.cryptog.projectmealprepandroid.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.cryptog.projectmealprepandroid.R
 import com.cryptog.projectmealprepandroid.data.model.Nutriment
 import kotlinx.android.synthetic.main.activity_detail_daily_portion.*
@@ -10,9 +13,13 @@ class DetailDailyPortionActivity : AppCompatActivity() {
 
     private lateinit var currentDailyPotion: ArrayList<Nutriment>
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_daily_portion)
+        supportActionBar!!.setDefaultDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
 
         val editingGroupDailyPotionList = arrayListOf(
             editingGroupVegetable,
@@ -80,5 +87,11 @@ class DetailDailyPortionActivity : AppCompatActivity() {
             val quantity = this.currentDailyPotion[key].quantity
             group.setValueInButtonNutriment(quantity)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        finish()
+
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/cryptog/projectmealprepandroid/ui/DetailMealPortionActivity.kt
+++ app/src/main/java/com/cryptog/projectmealprepandroid/ui/DetailMealPortionActivity.kt
@@ -1,10 +1,14 @@
 package com.cryptog.projectmealprepandroid.ui
 
+import android.annotation.SuppressLint
+import android.app.ActionBar
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import com.cryptog.projectmealprepandroid.R
 import com.cryptog.projectmealprepandroid.data.model.Meal
 import com.cryptog.projectmealprepandroid.ui.adapters.MealDetailAdapter
@@ -15,28 +19,31 @@ class DetailMealPortionActivity : AppCompatActivity() {
     private lateinit var currentMeal: Meal
     private var metalDetailAdapter = MealDetailAdapter()
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_meal_portion)
+        supportActionBar!!.run {
+            setDefaultDisplayHomeAsUpEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+        }
 
         if (intent.getSerializableExtra(DayFragment.EXTRA_MEAL) != null) {
             this.currentMeal = intent.getSerializableExtra(DayFragment.EXTRA_MEAL) as Meal
+            supportActionBar!!.setTitle(this.currentMeal.nameId)
         }
 
         val layoutManager = LinearLayoutManager(this)
         recycleViewDetailId.layoutManager = layoutManager
         this.metalDetailAdapter.update(currentMeal)
         recycleViewDetailId.adapter = metalDetailAdapter
+    }
 
-
-        btnCancel.setOnClickListener {
-            finish()
-        }
-        btnSave.setOnClickListener {
-            val intent = Intent()
-            intent.putExtra(DayFragment.EXTRA_MEAL, currentMeal)
-            setResult(Activity.RESULT_OK, intent)
-            finish()
-        }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val intent = Intent()
+        intent.putExtra(DayFragment.EXTRA_MEAL, currentMeal)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
